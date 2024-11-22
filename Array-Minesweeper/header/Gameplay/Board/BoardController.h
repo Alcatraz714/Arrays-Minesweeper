af37d34170def4d3d403a8bea434c144c4c7e93a
Rishi Saxena <rishi.saxena@gamestacy.com>
2024-11-22 14:38:32 +0530

    Added population code logic
    grid size is 9x9 we coded logic for 8x8

--- Array-Minesweeper/header/Gameplay/Board/BoardController.h
+++ Array-Minesweeper/header/Gameplay/Board/BoardController.h
@@ -10,13 +10,19 @@ namespace Gameplay
     {
         class BoardView;
 
+        enum class BoardState
+        {
+            FIRST_CELL,       // The state when the player opens first cell - populate cells after first cell is opened
+            PLAYING,          // The game is in progress.
+            COMPLETED,        // The game is over.
+        };
+
         class BoardController
         {
         public:
             static const int number_of_rows = 9;
             static const int number_of_colums = 9;
             static const int mines_count = 8;
-            
 
             BoardController();
             ~BoardController();
@@ -24,8 +30,14 @@ namespace Gameplay
             void initialize();
             void update();
             void render();
+
             void reset();
             void processCellInput(Cell::CellController* cell_controller, UI::UIElement::ButtonType button_type);
+
+            int getMinesCount();
+
+            BoardState getBoardState();
+            void setBoardState(BoardState state);
 
         private:
             BoardView* board_view;
@@ -36,12 +48,25 @@ namespace Gameplay
             void destroy();
             void resetBoard();
             void deleteBoard();
+
             //Cell functions
             void openCell(sf::Vector2i cell_position);
             void flagCell(sf::Vector2i cell_position);
             int flagged_cells;
 
-            int getMinesCount();
+            //Population of cells
+            BoardState board_state;
+            void populateBoard(sf::Vector2i cell_position);
+            void populateMines(sf::Vector2i cell_position);
+            void populateCells();
+            int countMinesAround(sf::Vector2i cell_position);
+            bool isValidCellPosition(sf::Vector2i cell_position);
+
+            // To generate random values.
+            std::default_random_engine random_engine;
+
+            // To give random seed to generator.
+            std::random_device random_device;
             
         };
     }
